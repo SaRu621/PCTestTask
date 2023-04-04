d190e6c0e92b78470c2caa133f660ecb42403236
SaRu621 <129864510+SaRu621@users.noreply.github.com>
2023-04-04 17:07:21 +0300

    Add files via upload

---
+++ Client.cpp
@@ -0,0 +1,51 @@
+//CSV library
+#include "/home/rus/Desktop/TestTask/Load.cpp"
+
+//Standart libraries
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <utility>
+#include <vector>
+#include <map>
+
+//Boost libraries, linking key: -lboost_system -pthread
+#include </usr/include/boost/asio.hpp>
+#include </usr/include/boost/asio/ts/buffer.hpp>
+#include </usr/include/boost/asio/ts/internet.hpp>
+
+boost::asio::io_context service;
+boost::system::error_code ec;
+boost::asio::ip::tcp::endpoint ep(boost::asio::ip::make_address("localhost", ec), 1110);
+
+void Query(std::string msg){
+    boost::asio::ip::tcp::socket sock(service);
+    sock.connect(ep);
+
+    sock.write_some(boost::asio::buffer(msg));
+    
+    sock.wait(sock.wait_read);
+    size_t bytes = sock.available();  
+
+    std::vector<char> Buff(bytes);
+    sock.read_some(boost::asio::buffer(Buff.data(), Buff.size()), ec);
+    
+    //std::cout << "Query: " << msg;
+    //std::cout << std::endl;
+    std::cout << "Result:\n";
+
+    for(auto c: Buff)
+        std::cout << c;
+
+    std::cout << "\n\n";
+    
+    sock.close();
+}
+
+int main(){ 
+    Query("get list");
+    Query("delete,/home/rus/Desktop/TestTask/covid_worldwide.csv");
+    Query("get list");
+    return 0;
+}
---
+++ Server.cpp
@@ -0,0 +1,83 @@
+//CSV library
+#include "/home/rus/Desktop/TestTask/Load.cpp"
+
+//Standart libraries
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <utility>
+#include <vector>
+#include <map>
+
+//Boost libraries, linking key: -lboost_system -pthread
+#include </usr/include/boost/asio.hpp>
+#include </usr/include/boost/asio/ts/buffer.hpp>
+#include </usr/include/boost/asio/ts/internet.hpp>
+#include </usr/include/boost/algorithm/string.hpp>
+
+boost::asio::io_context service; 
+boost::system::error_code ec;
+boost::asio::ip::tcp::acceptor acceptor(service, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), 1110));
+
+void handle_connection(Files& a){   
+    while(true){
+        boost::asio::ip::tcp::socket sock(service);
+        acceptor.accept(sock);
+        sock.wait(sock.wait_read);
+       
+        size_t bytes = sock.available();
+        std::vector<char> Buff(bytes);
+        sock.read_some(boost::asio::buffer(Buff.data(), Buff.size()), ec);
+        
+        std::string msg = "";
+
+        for(auto c: Buff)
+            msg = msg + c;
+
+        if(msg == "get list"){
+            msg = a.Get_List();
+        }
+        else
+        if(boost::algorithm::contains(msg, "sort")){
+            std::vector<std::string> for_sort = Split(msg, ',');
+            std::vector<std::string> Categories;
+            
+            for(int i = 2; i < for_sort.size(); i++)
+                Categories.push_back(for_sort[i]);
+
+            a.Sort(for_sort[1], Categories);
+            msg = a.Data[for_sort[1]].CSV_Into_String();
+        }
+        else
+        if(boost::algorithm::contains(msg, "get head")){
+            std::vector<std::string> get_header = Split(msg, ',');
+            msg = a.Get_Header(get_header[1]);
+        }
+        else
+        if(boost::algorithm::contains(msg, "delete")){
+            std::vector<std::string> del = Split(msg, ',');
+            a.Delete(del[1]);
+            msg = "Deleted!";
+        }
+
+        sock.write_some(boost::asio::buffer(msg));        
+        sock.close();
+    }
+}
+
+int main(){
+    CSV_Table file1("/home/rus/Desktop/TestTask/covid_worldwide.csv");
+    CSV_Table file2("/home/rus/Desktop/TestTask/prc_hpi_a__custom_3617733_page_linear.csv");
+    CSV_Table file3("/home/rus/Desktop/TestTask/test.csv");
+
+    Files Dataset;
+    Dataset.Add(file1);
+    Dataset.Add(file2);
+    Dataset.Add(file3);
+    
+    std::cout << "Server is active! :)\n";
+    handle_connection(Dataset); 
+    
+    return 0;
+}
